88df928072b46d3eaf4ec0ffe5895b59983ee0d7
Lucas Curran <ljc51204@gmail.com>
2023-09-29 13:25:06 -0400

    feat: code

--- socket/index.js
+++ socket/index.js
@@ -1,40 +1,46 @@
 module.exports = (io) => {
-  io.on("connection", (socket) => {
-    console.log("new connection");
+  io.on('connection', (socket) => {
+    console.log('new connection');
 
-    socket.on("disconnect", () => {
-      console.log("disconnected");
+    socket.on('disconnect', () => {
+      console.log('disconnected');
     });
 
-    socket.on("create-lobby", (roomId, user) => {
-      let rooms = Array.from(io.of("/").adapter.rooms.keys());
+    socket.on('create-lobby', (roomId) => {
+      let rooms = Array.from(io.of('/').adapter.rooms.keys());
       if (rooms.includes(roomId)) {
-        socket.emit("failed-create");
+        socket.emit('failed-create');
         return;
       }
       socket.join(roomId);
-      socket.emit("lobby-created");
+      socket.emit('lobby-created');
     });
 
-    socket.on("join-lobby", (roomId, user) => {
-      let rooms = Array.from(io.of("/").adapter.rooms.keys());
+    socket.on('join-lobby', (roomId, user) => {
+      let rooms = Array.from(io.of('/').adapter.rooms.keys());
       if (!rooms.includes(roomId)) {
-        socket.emit("failed-join", roomId);
+        socket.emit('failed-join', roomId);
         return;
       }
       socket.join(roomId);
-      socket.to(roomId).emit("user-connected", user.nickname);
-      socket.emit("lobby-joined");
+      socket.to(roomId).emit('user-connected', user.nickname);
+      socket.emit('lobby-joined');
     });
 
-    socket.on("start", (data) => {
-      socket.broadcast.emit("start", data);
+    socket.on('nickname-picked', (roomId, nickname) => {
+      console.log(roomId);
+      console.log(nickname);
+      socket.to(roomId).emit('player-picked-nickname', nickname);
     });
-    socket.on("draw", (data) => {
-      socket.broadcast.emit("draw", data);
+
+    socket.on('start', (data) => {
+      socket.broadcast.emit('start', data);
     });
-    socket.on("end", () => {
-      socket.broadcast.emit("end");
+    socket.on('draw', (data) => {
+      socket.broadcast.emit('draw', data);
+    });
+    socket.on('end', () => {
+      socket.broadcast.emit('end');
     });
   });
 };
